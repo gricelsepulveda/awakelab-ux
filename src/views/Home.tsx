72f50792ca6fce18c9fdae90d27e2e0c20817ec7
Slowpokita <gr3c2l@gmail.com>
2020-04-11 18:41:10 -0400

    Wip markup + scss

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -11,6 +11,75 @@ const Home:React.FunctionComponent = () => {
 
   return (
     <>
+      <nav className="navbar">
+        <a>
+          <img className="logo-awakelab" src="https://gricel.sfo2.digitaloceanspaces.com/awakelab/logo-awakelab-inverse.svg" alt="logo awakelab" title="logo awakelab"/>
+        </a>
+        <div className="welcome">
+          <p className="welcome-phrase">
+            ¡Bienvenido <span className="expression">awaker</span>!
+          </p>
+          <ul className="progress-indicators">
+            <li className="progress-indicator module">
+                <button className="p-indicator-number">
+                  1
+                </button>
+                <span className="p-indicator-name">
+                  Módulo
+                </span>
+            </li>
+            <li className="progress-indicator class">
+                <button className="p-indicator-number">
+                  1
+                </button>
+                <span className="p-indicator-name">
+                  Clase
+                </span>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <div className="body-content">
+        <div className="body-content-top">
+          <nav className="modules-selector">
+            <ul className="m-selector-wrapper">
+              <li className="module active">
+                <button className="btn-module">1</button>
+              </li>
+              <li className="module">
+                <button className="btn-module">2</button>
+              </li>
+              <li className="module">
+                <button className="btn-module">3</button>
+              </li>
+              <li className="module">
+                <button className="btn-module">4</button>
+              </li>
+              <li className="module">
+                <button className="btn-module">5</button>
+              </li>
+            </ul>
+          </nav>
+          <h1>Módulo 1: <span>diseño visual y web</span></h1>
+        </div>
+        <div className="body-content-center">
+          <ul className="module-tags">
+            <li className="tag">semiótica</li>
+            <li className="tag">color</li>
+            <li className="tag">tipografía</li>
+            <li className="tag">herramientas de software</li>
+          </ul>
+          <ul className="submodule-list">
+            <h2>Lenguaje básico de diseño</h2>
+            <li className="submodule">Proximidad</li>
+            <li className="submodule">Espacio</li>
+            <li className="submodule">Alineación</li>
+            <li className="submodule">Contraste</li>
+            <li className="submodule">Repetición</li>
+          </ul>
+          {/*https://gricel.sfo2.digitaloceanspaces.com/awakelab/puzzle-white-center.svg*/}
+        </div>
+      </div>
     </>
   )
 }
